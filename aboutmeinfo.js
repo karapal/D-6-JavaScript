9dd1d4ca99cd8ca4216194a26014769dbaa39e92
karapal <106742331+karapal@users.noreply.github.com>
2022-11-28 18:22:35 +0100

    .

---
+++ aboutmeinfo.js
@@ -0,0 +1,17 @@
+
+
+let firtName = 'Lydia' ;
+let lastName = '\n Karapatsakidi';
+let fullName = firtName +  lastName
+
+
+console.log ("Hello, my name is", fullName );
+
+let currentAge = '27';
+let currentDate = '2022';
+let birthYear = currentAge - currentDate;
+
+console.log ( "My birth year is", birthYear );
+
+
+
